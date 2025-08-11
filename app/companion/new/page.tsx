9528b197e90421dab7c33de0dd0ec13f271b634b
Daniel Canoy <canoydaniel06@gmail.com>
2025-08-11 20:33:37 +0800

    initial commit

---
+++ app/companion/new/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewCompaninon = () => {
+  return <div></div>;
+};
+
+export default NewCompaninon;
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignInPage = () => {
+  return (
+    <div>
+      <h1>Sign in</h1>
+    </div>
+  );
+};
+
+export default SignInPage;
